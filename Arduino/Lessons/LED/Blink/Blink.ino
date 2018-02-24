7b0e85a7a5fbd6abc8876bc834718245e424e109
Chris Wilcox <rcwilcox59@gmail.com>
2018-02-24 16:59:18 -0500

    Initial commit
    Blink a single LED

---
+++ Arduino/Lessons/LED/Blink/Blink.ino
@@ -0,0 +1,30 @@
+const int ledPin = 9;   // pushbutton pin
+
+void setup()
+{
+  // Set up the pushbutton pins to be an input:
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+  Serial.print("Setup\n");
+}
+
+void loop()
+{
+ 
+    digitalWrite(ledPin, HIGH);  //    turn on LED
+    Serial.print("ON\n");
+    delay(500);
+    
+    digitalWrite(ledPin, LOW);  //    turn off LED
+    Serial.print("OFF\n");
+    delay(500);
+
+    analogWrite(ledPin,128);
+    delay(500);
+}
+  
+  
+
+
+
+
